38ffeea0f5078fad8b3b586ecd460b199815421f
vedagya2007 <74814960+vedagya2007@users.noreply.github.com>
2020-12-23 19:50:33 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,23 @@
+class chain
+
+{
+    constructor(body1,body2){
+    var options={
+     bodyA:body1,
+     bodyB:body2,
+     stifness:0.04,
+     length:10
+    }
+    this.chain=Constraint.create(options);
+    World.add(world,this.chain);
+    
+    }
+    display()
+    {
+        var pointa=this.chain.bodyA.position;
+        var pointb=this.chain.bodyB.position;
+        strokeWeight(4);
+        line(pointa.x,pointa.y,pointb.x,pointb.y);
+    }
+
+}
